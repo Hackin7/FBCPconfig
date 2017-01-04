8ee724a04d8b653f9c7ce38ebb8eca76cc27b3cb
Hackin7 <zunmun@gmail.com>
2017-01-04 18:39:02 +0800

    Add files via upload

---
+++ Variables.py
@@ -0,0 +1,7 @@
+currentresolution = (640, 480)
+resolutions = []
+resolutions.append((1280, 768))
+resolutions.append((320, 240))
+resolutions.append((640, 480))
+resolutions.append((240, 320))
+resolutions.append((1280, 960))
---
+++ setup.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+echo "Open README first
+Choose:
+  1 to install only fbcp
+  2 to install only FBCPconfig software for BlockComPi (install fbcp first)"
+read option
+#####fbcp###################################################################
+if [ $option -eq 1 ]
+then
+cd /tmp
+sudo apt-get install cmake
+git clone https://github.com/tasanakorn/rpi-fbcp
+cd rpi-fbcp/
+mkdir build
+cd build
+cmake ..
+make
+sudo install fbcp /usr/local/bin/fbcp
+cd -
+echo 'Done installing fbcp'
+####HDMIconfig####################################################################
+elif [ $option -eq 2 ]
+then
+echo "Enter the directory the App.py file of the BlockComPi software is in"
+echo "eg. /home/pi/BlockComPiSoftware/App.py"
+read Appdir
+echo "Enter the App folder (after copying it over)"
+echo "eg. /home/pi/FBCPconfig"
+read Installdir
+echo "def app():
+  os.system('cd "$Installdir" && python FBCPconfig.py &')
+def exit():
+  os.system('pkill fbcp')
+  os.system('pkill -f FBCPconfig.py')
+  os.system('rmmod fb_ili9340')# Change rotation
+  os.system('rmmod fbtft_device')# Change rotation
+  os.system('modprobe fbtft_device name=pitft rotate=180')# Change rotation
+  os.system('adafruit-pitft-touch-cal -f -r 180')# Change rotation
+FBCPconf = ('FBCPconf', orange, 24, app, exit)
+programs.append(FBCPconf)" >> $Appdir
+echo 'Done installing FBCPconfig software for BlockComPi'
+fi
